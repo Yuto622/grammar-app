662f40fa827dffc407000424f362e5ce32c2dd39
Hoang Tung <tunghv@amcolab.vn>
2024-07-23 17:09:59 +0700

    add admin to project

--- users/managers.py
+++ users/managers.py
@@ -15,3 +15,13 @@ class CustomUserManager(BaseUserManager):
         user.set_password(password)
         user.save()
         return user
+
+    def create_superuser(self, email, username, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
